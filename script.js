0cfbb2e535d406a26db5e3770518759ba3d5b35a
Luis Miguel <miguepach100@gmail.com>
2025-01-07 00:52:38 -0500

    Quinto commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let p = document.getElementById("p")
 function namek(){
     let nom = prompt("Tu nombre")
-    p.innerText = `Hola, ${nom} tu ere la polla`
+    p.innerText = `Hola, ${nom}. tu ere la polla. Yo ya estuve en estos cerros <))))`
 }
