fe98fe266ecd9caa08ad9347d7f9a451868c68f3
Luis Miguel <miguepach100@gmail.com>
2025-01-10 11:40:29 -0500

    Cambio de color al h1

--- script.js
+++ script.js
@@ -7,8 +7,13 @@ document.body.addEventListener("keydown", (event) => {
             let g = Math.round(Math.random() * 256)
             let b = Math.round(Math.random() * 256)
             document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+
+            let rh = Math.round(Math.random() * 256)
+            let gh = Math.round(Math.random() * 256)
+            let bh = Math.round(Math.random() * 256)
+            document.getElementById("h").style.color = `rgb(${rh}, ${gh}, ${bh})`
     
-        }, 500)
+        }, 1000)
     }
 
 })
