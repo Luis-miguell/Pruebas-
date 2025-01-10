f3fa76403676543e1243ca4c9a29fb7574e23655
Luis Miguel <miguepach100@gmail.com>
2025-01-10 00:50:06 -0500

    Proyecto

---
+++ script.js
@@ -0,0 +1,14 @@
+document.body.addEventListener("keydown", (event) => {
+    
+    if (event.key != "Enter"){
+        setInterval(() => {
+
+            let r = Math.round(Math.random() * 256)
+            let g = Math.round(Math.random() * 256)
+            let b = Math.round(Math.random() * 256)
+            document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+    
+        }, 1000)
+    }
+
+})
